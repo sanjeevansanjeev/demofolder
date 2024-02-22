203f1179d60d876ddb652544524f604a613deecb
Sanjeev <154337538+sanjeevansanjeev@users.noreply.github.com>
2024-02-22 10:57:00 +0530

    Add files via upload

---
+++ DbpgmApplication.java
@@ -0,0 +1,14 @@
+package com.sanjeev.crudoperation.Dbpgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbpgmApplication.class, args);
+	}
+
+}
+
